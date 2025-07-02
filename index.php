9ec454d9496d21ccf351dc07a1ebeae8709578e0
Mouadlamsila <mouad.lamssila@gmail.com>
2025-07-02 18:27:05 +0100

    hh

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php
+include("connect.php");
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>My PHP Site</title>
+</head>
+<body>
+    <h1>مرحبا بك في موقعي 🎉</h1>
+
+    <?php
+    echo "<p>تم الاتصال بقاعدة البيانات بنجاح!</p>";
+    ?>
+</body>
+</html>
